d45be87b159f2ce322b4c2b97cb7a9b35b479e88
Sunand Pradhan <116841983+sunandd@users.noreply.github.com>
2023-06-16 11:48:33 +0530

    Add files via upload

---
+++ NumberGuissing.py
@@ -0,0 +1,20 @@
+print("enter your name")
+var5 = input()
+print("hello",var5)
+print("enter your age")
+var1 = 18
+var2 = 100
+var4 = 0
+var3 = int(input())
+print(var5)
+if var3>var1:
+   print("you are elegible for driving")
+elif var3==var1:
+    print("you can come to our office to veryfy")
+else:
+    print("you are not elegible for driving")
+    print("you have to wait for",var1-var3,"more years")
+if var3>var2:
+    print("but you dont need a driving licence for","drive your car in heaven")
+elif var3==var4:
+    print("get birth first")
